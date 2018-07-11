d54b39d7cb778397c7b21e3dd621bd2dfa96cda7
gena-rybin <Gennadiy.Rybin@dataart.com>
2018-07-11 19:21:18 +0300

    refactoring: admin-employee adding reactive forms, finished

--- src/app/pages/admin/a-employees/a-employees-mod-reactive-f/a-employees-mod-reactive-f.component.ts
+++ src/app/pages/admin/a-employees/a-employees-mod-reactive-f/a-employees-mod-reactive-f.component.ts
@@ -4,6 +4,7 @@ import {EmployeeModel} from '../../../../models/employee.model';
 import {BackendService} from '../../../../services/backend.service';
 import {ALL_LOCATIONS, ALL_POSITIONS, ALL_ROLES} from '../../../../models/const/app.constants';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'pt-a-employees-mod-reactive-form',
@@ -12,6 +13,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class AEmployeesModReactiveFComponent implements OnInit, OnDestroy {
   newAdminEmployeeForm: FormGroup;
+  private locationSubscription: Subscription;
+  initialBirthday = '2018-04-18T07:21:01.477Z';
 
   newEmployees = new EmployeeModel();
   allLocations = ALL_LOCATIONS;
@@ -46,23 +49,55 @@ export class AEmployeesModReactiveFComponent implements OnInit, OnDestroy {
 
   initReactiveForm() {
     this.newAdminEmployeeForm = this.fb.group({
-      name: ['emptyName', Validators.compose([Validators.required])],
-      description: [null, Validators.compose([
-        Validators.required, Validators.minLength(1), Validators.pattern(/^[А-яa-z0-9 ]+$/i)
-      ])],
-      customerName: [null, Validators.compose([Validators.required])],
-      startDate: [null, Validators.compose([Validators.required])],
-      endDate: [null, Validators.compose([Validators.required])],
-      imageUrl: [null],
-      tickets: [[]],
-      id: [null]
+      First: [null, Validators.compose([Validators.required])],
+      Last: [null, Validators.compose([Validators.required])],
+      Birthday: [this.initialBirthday],
+      Email: [null, Validators.compose([Validators.required])],
+      Password: [null, Validators.compose([Validators.required, Validators.minLength(6)])],
+      Address: [null, Validators.compose([Validators.required])],
+      Skype: [null, Validators.compose([Validators.required])],
+      Phone: [null, Validators.compose([Validators.required])],
+      ImageUrl: [null],
+      PositionId: [this.currentPosition],
+      Position: [{
+        Name: this.allPositions.filter(p => +p.id === this.currentPosition)[0].name,
+        Id: this.currentPosition
+      }, Validators.compose([Validators.required])],
+      LocationId: [this.currentLocation],
+      Location: [{
+        Name: this.allLocations.filter(l => +l.id === +this.currentLocation)[0].name,
+        Id: this.currentLocation
+      }, Validators.compose([Validators.required])],
+      Projects: [[]],
+      Roles: [[this.currentRole]],
+      FullName: [null],
+      Id: [null]
     });
+  }
+
+  fillLocation(id: number) {
+    this.newAdminEmployeeForm.value.LocationId = +id;
+    this.newAdminEmployeeForm.value.Location.Id = +id;
+    const location = this.allLocations.filter(l => +l.id === +id)[0];
+    this.newAdminEmployeeForm.value.Location['Name'] = location ? location.name : '';
+  }
+  fillPosition(id: number) {
+    this.newAdminEmployeeForm.value.PositionId = +id;
+    this.newAdminEmployeeForm.value.Position.Id = +id;
+    const position = this.allPositions.filter(p => +p.id === +id)[0];
+    this.newAdminEmployeeForm.value.Position['Name'] = position ? position.name : '';
+  }
+  fillRole(id: number) {
+    this.newAdminEmployeeForm.value.Roles = [+id];
+  }
+  fillProjects(id: number) {
+    this.newAdminEmployeeForm.value.Projects = [+id];
   }
 
   showDialogToAdd() {
     this.newEmployees = new EmployeeModel();
     this.newEmployees.LocationId = +this.currentLocation;
-    this.newEmployees.Birthday = '2018-04-18T07:21:01.477Z';
+    this.newEmployees.Birthday = this.initialBirthday;
     this.newEmployees.ImageUrl = 'undefined';
     this.newEmployees.Password = '111111';
   }
@@ -73,22 +108,16 @@ export class AEmployeesModReactiveFComponent implements OnInit, OnDestroy {
   }
 
   public save() {
-    this.newEmployees.LocationId = +this.currentLocation;
-    this.newEmployees.Projects = [];
-    this.newEmployees.Projects.push(+this.currentProjectId);
-    this.newEmployees.Roles = [];
-    this.newEmployees.Roles.push(+this.currentRole);
-    this.newEmployees.PositionId = +this.currentPosition;
-
-    this.backendService.addEmployee(this.newEmployees)
-      .takeWhile(() => this.alive)
-      .subscribe((res) => {
-        this.successSave.emit(true);
-        this.cancel();
-      }, (err) => {
-        console.log(err);
-      } );
-
+    if (this.newAdminEmployeeForm.valid) {
+      this.backendService.addEmployee(this.newAdminEmployeeForm.value)
+        .takeWhile(() => this.alive)
+        .subscribe((res) => {
+          this.successSave.emit(true);
+          this.cancel();
+        }, (err) => {
+          console.log(err);
+        } );
+    }
   }
 
 }
